729742b0c123c032369a462d4507ad75b855663c
Barkha Yadav <barkhayadav125@gmail.com>
2023-10-08 10:09:24 +0530

    Added password strength validation

--- script1.js
+++ script1.js
@@ -1,8 +1,40 @@
 const isLoggedIn = sessionStorage.getItem("isLoggedIn");
 
-if (!isLoggedIn && window.location.pathname !== "/login1.html" && window.location.pathname !== "/signup1.html") {
-    window.location.href = "login1.html";
-}
+// if (!isLoggedIn && window.location.pathname !== "/login1.html" && window.location.pathname !== "/signup1.html") {
+//     window.location.href = "login1.html";
+// }
+
+//password validation for sign up
+
+var pass=document.getElementById("password");
+var msg=document.getElementById("msg");
+var str=document.getElementById("str");
+
+pass.addEventListener('input',()=>{
+    if(pass.value.length>0)
+    {
+        msg.style.display="block";
+    }
+    else{
+        msg.style.display="none";
+    }
+    if(pass.value.length<4)
+    {
+        str.innerHTML="weak";
+        pass.style.color="red";
+    }
+    else if(pass.value.length>=4 && pass.value.length<8)
+    {
+        str.innerHTML="medium";
+        pass.style.color="yellow";
+    }
+    else if(pass.value.length>=8)
+    {
+        str.innerHTML="strong";
+        pass.style.color="green";
+    }
+})
+
 
 document.getElementById("loginForm").addEventListener("submit", function (event) {
     event.preventDefault();
